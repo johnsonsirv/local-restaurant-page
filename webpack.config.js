27aec9119ccbffed385a1650b05f45660c63b4f9
Victor Johnson <johnsonsirv@gmail.com>
2019-12-12 17:23:07 +0100

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js',
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    publicPath: '/',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Ntachi-Osa Restaurant',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+};
